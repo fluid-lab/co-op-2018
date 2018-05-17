afe73d5f2deb08292daf44789565bb920c996b5b
Yehchan <Yehchan.P@gmail.com>
2018-05-17 14:46:14 -0400

    Formating and general fixes
    Added comments.
    Fixed the spacing to be more consistant and better looking.
    New background image.

--- Experimental/scanning.js
+++ Experimental/scanning.js
@@ -1,144 +1,113 @@
 var canvasWidth = 1280;
 var canvasHeight = 720;
 
-var selected = false;
-var scanSpeed = 60;
+var scanSpeed = 60; //Timer in
 var speedCounter = 0;
-var speedLimit = 1500/scanSpeed;
+var speedLimit = 1500 / scanSpeed;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var stage = 1; //stage 1 is the first choice, stage 2 is the second choice
-
-//var drill;
-//var scannedDrill;
-//var background;
-//var rightArrow;
-//var leftArrow;
-//var leftArrowScanner;
-//var rightArrowScanner;
-
-var imageSelection = 1;
+var stage1Choice = 0; //Choice chosen in stage 1
+var stage2Choice = 0; //Choice chosen in stage 2
+var imageSelection = 1; //What image is highlighted
 var xPos = 50;
-var yPos = 0;
 
-//var miningPlanetHeight = (1280/1600) * 3000;
-//var miningPlanetWidth = 1280;
-//var bottomOfPlanet = ((1280/1600) * 2100) * -1;
-
- window.onload = function (){
-    choice = this.document.getElementById ("Choice");
-    hChoice = this.document.getElementById ("HChoice");
-    background = this.document.getElementById ("background");
-    back = this.document.getElementById ("back");
-    hBack = this.document.getElementById ("HBack");
-
-    ctx.drawImage(background,0, 0,);
+window.onload = function() {
+    //loads images
+    choice = this.document.getElementById("Choice");
+    hChoice = this.document.getElementById("HChoice");
+    background = this.document.getElementById("background");
+    back = this.document.getElementById("back");
+    hBack = this.document.getElementById("HBack");
+    //draws imaages
+    ctx.drawImage(background, 0, 0);
     ctx.drawImage(choice, 50, 50, 300, 300);
-    ctx.drawImage(choice,350, 50, 300, 300);
-    ctx.drawImage(back,650, 50, 300, 300);
-
-    var id = setInterval(animation,scanSpeed);
- };
- function animation(){
-    if(stage == 1){
-        if(speedCounter === speedLimit){
-           imageSelection++;
-           if(imageSelection >= 4){
-             imageSelection = 1;
-           }
-           speedCounter = 0;
+    ctx.drawImage(choice, 350, 50, 300, 300);
+    ctx.drawImage(back, 650, 50, 300, 300);
+    //repeats animation at a fixed rate
+    var id = setInterval(animation, scanSpeed);
+}
+function animation() {
+    if (stage == 1) {
+        if (speedCounter === speedLimit) {
+            imageSelection++;//If the counter reaches a point, it goes to the next image
+            if (imageSelection >= 4) {//If the number is too high, it goes back to the beginning
+                imageSelection = 1;
+            }
+            speedCounter = 0;
         }
-        if(imageSelection === 1){
+        if (imageSelection === 1) {//Sets the position where something happens
             xPos = 50;
-        }
-        else if(imageSelection === 2){
-           xPos = 350;
+        } else if (imageSelection === 2) {
+            xPos = 350;
         }
         speedCounter++;
     }
-    if(selected === true){
-//        if(!(yPos <= bottomOfPlanet)){
-//            yPos = yPos - 10;
-//        }
-        if(speedCounter === speedLimit){
+    ctx.drawImage(background, 0, 0);//refreshes the background
+    if (stage == 1) {
+        ctx.drawImage(choice, 50, 50, 300, 300);
+        ctx.drawImage(choice, 350, 50, 300, 300);
+        ctx.drawImage(back, 650, 50, 300, 300);
+        if (imageSelection === 3) {
+            ctx.drawImage(hBack, 650, 50, 300, 300);
+        } else if (imageSelection <= 2) {
+            ctx.drawImage(hChoice, xPos, 50, 300, 300);
+        }
+    }
+    if (stage == 2) {//similar to stage 1
+        ctx.drawImage(choice, 50, 450, 300, 300);
+        ctx.drawImage(choice, 350, 450, 300, 300);
+        ctx.drawImage(back, 650, 450, 300, 300);
+        if (speedCounter === speedLimit) {
             imageSelection++;
-            speedCounter = 0;
-            if(imageSelection === 4){
+            if (imageSelection >= 4) {
                 imageSelection = 1;
             }
-         }
-         speedCounter++;
+            speedCounter = 0;
+        }
+        if (imageSelection === 1) {
+            xPos = 50;
+        } else if (imageSelection === 2) {
+            xPos = 350;
+        } else if (imageSelection === 3) {
+            xPos = 650;
+        }
+        speedCounter++;
+        if (imageSelection === 3) {
+            ctx.drawImage(hBack, 650, 450, 300, 300);
+        } else {
+            ctx.drawImage(hChoice, xPos, 450, 300, 300);
+        }
     }
-    ctx.drawImage(background,0,yPos);
-    if(stage == 1){
-      ctx.drawImage(choice, 50, 50, 300, 300);
-      ctx.drawImage(choice,350, 50, 300, 300);
-      ctx.drawImage(back,650, 50, 300, 300);
-      if (imageSelection === 3){
-        ctx.drawImage(hBack,650, 50, 300, 300);
-      }else if(imageSelection <= 2){
-        ctx.drawImage(hChoice, xPos, 50, 300, 300);
-      }
-    }
-    if(stage == 2){
-      ctx.drawImage(choice, 50, 450, 300, 300);
-      ctx.drawImage(choice,350, 450, 300, 300);
-      ctx.drawImage(back,650, 450, 300, 300);
-      if(speedCounter === speedLimit){
-         imageSelection++;
-         if(imageSelection >= 4){
-           imageSelection = 1;
-         }
-         speedCounter = 0;
-      }
-          if(imageSelection === 1){
-              xPos = 50;
-          }
-          else if(imageSelection === 2){
-             xPos = 350;
-          }
-          else if(imageSelection === 3){
-             xPos = 650;
-          }
-          speedCounter++;
-
-      if (imageSelection === 3){
-        ctx.drawImage(hBack,650, 450, 300, 300);
-      }else{
-        ctx.drawImage(hChoice, xPos, 450, 300, 300);
-      }
-
-    }
-//    if(selected === true){
-//        ctx.drawImage (leftArrow, (xPos - 25), 200, 100, 50);
-//            ctx.drawImage (leftArrowScanner, (xPos - 25), 200, 100 ,50);
-//        if(imageSelection === 1){
-//            ctx.drawImage (rightArrowScanner, (xPos + 200), 200, 100, 50);
-//        }
-//    }
 }
- window.onkeyup = function(e){
-     if(e.key === " "){
-         if(stage == 1){
-           if (imageSelection!=3) {
-             stage = 2;
-             imageSelection = 1;
-             speedCounter = 0;
-           }else {
-             stage = 0
-           }
-
-         }else if(stage == 2){
-           if (imageSelection!=3) {
-             stage = 3;
-             imageSelection = 1;
-             speedCounter = 0;
-           }else {
-             stage = 1
-           }
-//            if(selected === false){
-//                selected = true;
-//            }
-         }
-     }
- }
+window.onkeyup = function(e) {
+    if (e.key === " ") {//triggers when the space key is let go
+        if (stage == 1) {
+            if (imageSelection != 3) {
+                if (imageSelection == 2) {
+                    stage1Choice = 2;
+                } else if (imageSelection == 1) {
+                    stage1Choice = 1;
+                }
+                stage = 2;
+                imageSelection = 1;
+                speedCounter = 0;
+            } else {
+                stage = 0
+            }
+        } else if (stage == 2) {
+            if (imageSelection != 3) {
+                if (imageSelection == 2) {
+                    stage2Choice = 2;
+                } else if (imageSelection == 1) {
+                    stage2Choice = 1;
+                }
+                stage = 3;
+                imageSelection = 1;
+                speedCounter = 0;
+            } else {
+                stage = 1
+            }
+        }
+    }
+}
